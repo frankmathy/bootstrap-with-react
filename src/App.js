191938a687d25c378af6e1e3284208746ff06796
Frank Mathy <frankmathy@gmail.com>
2020-12-31 01:32:06 +0100

    Finished tutorial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React from 'react';
+// See Youtube tutorial https://www.youtube.com/watch?v=NlZUtfNVAkI
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
 
+import { Container, Row, Col } from 'react-bootstrap';
+import { PizzaCard } from './components/PizzaCard';
+import { Confirmation } from './components/Confirmation';
+import pizzas from './data';
+
 function App() {
-  return <p>React Bootstrap</p>;
+  const [ordered, setOrdered] = useState(false);
+
+  function displayConfirmation() {
+    setOrdered(true);
+    setTimeout(() => {
+      setOrdered(false);
+    }, 3000);
+  }
+
+  return (
+    <>
+      <Container>
+        {ordered && <Confirmation toggle={setOrdered} />}
+        <Row>
+          {pizzas.map(data => (
+            <Col xs={3} className="mb-5" key={`${data.id}`}>
+              <PizzaCard data={data} setOrdered={displayConfirmation} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </>
+  );
 }
 
 export default App;
